b22ec44f32fd7ceb6cf298742ebbbb0f894815f3
MuhammadHammadFaisal <hammadbinfaisal@gmail.com>
2026-02-10 18:09:51 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -145,8 +145,8 @@ def main():
         st.image("assets/Sticker.png", use_container_width=True)
     with col_text:
         st.markdown("""
-        <h1 style="color#FFFFFF; font-size:46px; margin-bottom:6px;">MHF Civil Calc</h1>
-        <p style="#E2E8F0; font-size:18px; line-height:1.5; max-width:700px;">
+        <h1 style="color:#FFFFFF; font-size:46px; margin-bottom:6px;">MHF Civil Calc</h1>
+        <p style="color:#E2E8F0; font-size:18px; line-height:1.5; max-width:700px;">
             Civil Engineering Calculation Workspace
         </p>
         <p style="color:#E2E8F0; font-size:14px; max-width:700px;">
@@ -209,6 +209,7 @@ def main():
 # =========================================================
 if __name__ == "__main__":
     main()
+
 
 
 
