a32ddd55e4ce831e8b15f5077951fc989a65d874
Muhammad Hammad <hammadbinfaisal@gmail.com>
2026-03-01 14:36:16 +0300

    Update Stability_of_Slopes.py

--- topics/Stability_of_Slopes.py
+++ topics/Stability_of_Slopes.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import math
-from theme import write_text, glass_box, glass_table, apply_theme
+from theme import write_text, glass_box, glass_table
 # =========================================================
 # HELPER FUNCTIONS
 # =========================================================
