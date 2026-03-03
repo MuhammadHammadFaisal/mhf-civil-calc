63e33b0438352596894a3413f06bc0cf60ef635c
Muhammad Hammad <hammadbinfaisal@gmail.com>
2026-03-03 16:55:27 +0300

    Update axial_analysis.py

--- topics/concrete/axial_analysis.py
+++ topics/concrete/axial_analysis.py
@@ -167,8 +167,8 @@ def app():
         core_diameter_input,
         )
         
-        with st.container(border=True):
-            st.markdown(step_md)
+            with st.container(border=True):
+                st.markdown(step_md)
 
 if __name__ == "__main__":
     app()
