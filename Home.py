5a1c9b6eaeaab82baff080669586582d68628dbe
Muhammad Hammad <hammadbinfaisal@gmail.com>
2026-03-22 17:35:21 +0300

    Refactor Home.py by removing unused code

--- Home.py
+++ Home.py
@@ -32,15 +32,6 @@ def get_active_modules():
 # Main Application
 # =========================================================
 def main():
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3NKWXNDFY7"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-3NKWXNDFY7');
-</script>
     # ------------------------- HEADER -------------------------
     col_logo, col_text = st.columns([1, 3])
     with col_logo:
