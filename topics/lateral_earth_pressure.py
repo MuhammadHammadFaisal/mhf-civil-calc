bcfe8d9fa958842686fa8b5942bd85565e4904b9
Muhammad Hammad <hammadbinfaisal@gmail.com>
2026-03-28 15:10:57 +0300

    Update lateral_earth_pressure.py

--- topics/lateral_earth_pressure.py
+++ topics/lateral_earth_pressure.py
@@ -673,7 +673,7 @@ def app():
 
             # Wall back face
             ax_w.plot([0, top_x_full], [0, H_c], color="black", linewidth=2.2, zorder=4)
-
+            if cw["valid"]:
                 x_c_full = cw["x_c_full"]
                 y_c_full = cw["y_c_full"]
                 x_d = cw["x_d"]
